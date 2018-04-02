00e60f00bd8a7162928873d83fb56c4355b629fd
berkandemirel <demirelberkan@gmail.com>
2018-04-02 21:03:11 +0300

    preprocessing directory is added

---
+++ preprocess/latlon2grid.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# --------------------------------------------------------
+# convert latitude longitude values to the grid coordinate system
+# --------------------------------------------------------
