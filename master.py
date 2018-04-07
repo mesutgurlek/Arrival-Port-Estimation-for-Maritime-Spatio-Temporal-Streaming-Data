d31635235c6b004993fc6287ad152b39bed8d5ce
berkandemirel <demirelberkan@gmail.com>
2018-04-07 21:46:00 +0300

    unnec files are deleted

--- master.py
+++ master.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     values[:, 6] = encoder.fit_transform(values[:, 6])
     values[:, 5] = encoder.fit_transform(values[:, 5])
     # ensure all data is float
-    values = values.astype('float32')
+    #values = values.astype('float32')
     # normalize features
     scaler = MinMaxScaler(feature_range=(0, 1))
     scaled = scaler.fit_transform(values[:,[0,1,2,3,4,5]])
@@ -70,11 +70,13 @@ if __name__ == "__main__":
     print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
 
     model = Sequential()
+
+    model = Sequential()
     model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
     model.add(Dense(1))
-    model.compile(loss='mae', optimizer='adam')
+    model.compile(loss='mean_squared_error', optimizer='adam')
     # fit network
-    history = model.fit(train_X, train_y, epochs=1, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
+    history = model.fit(train_X, train_y, epochs=10, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
     # plot history
     pyplot.plot(history.history['loss'], label='train') 
     pyplot.plot(history.history['val_loss'], label='test')
@@ -83,18 +85,3 @@ if __name__ == "__main__":
 
     # make a prediction
     yhat = model.predict(test_X)
-    '''
-    test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
-    # invert scaling for forecast
-    inv_yhat = concatenate((yhat, test_X[:, 1:]), axis=1)
-    inv_yhat = scaler.inverse_transform(inv_yhat)
-    inv_yhat = inv_yhat[:, 0]
-    # invert scaling for actual
-    test_y = test_y.reshape((len(test_y), 1))
-    inv_y = concatenate((test_y, test_X[:, 1:]), axis=1)
-    inv_y = scaler.inverse_transform(inv_y)
-    inv_y = inv_y[:,0]
-    # calculate RMSE
-    rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
-    print('Test RMSE: %.3f' % rmse)
-    '''
